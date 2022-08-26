005170dceec0361a911fa57de25df66bcf9e3203
Teelirium <angryzombi01@gmail.com>
2022-08-26 22:01:26 +0500

    inline css & js into html

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,24 @@
 var gulp = require('gulp'),
 cleanCSS = require('gulp-clean-css'),
 terser = require('gulp-terser'),
-htmlMin = require('gulp-htmlmin');
+htmlMin = require('gulp-htmlmin'),
+inlineSource = require('gulp-inline-source');
 
-function css() {
+function stuff() {
     gulp.src('./src/styles.css')
     .pipe(cleanCSS())
-    .pipe(gulp.dest('.'));
-}
+    //.pipe(gulp.dest('.'));
 
-function js() {
     gulp.src('./src/scripts.js')
     .pipe(terser())
-    .pipe(gulp.dest('.'));
-}
+    //.pipe(gulp.dest('.'));
 
-function html() {
     gulp.src('./src/index.html')
+    .pipe(inlineSource())
     .pipe(htmlMin({
         collapseWhitespace: true
     }))
     .pipe(gulp.dest('.'));
 }
 
-exports.default = gulp.parallel(css, js, html)
+exports.default = gulp.task('default', stuff);
